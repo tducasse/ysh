273bde89ac2b367c3bc795a352c9091239bd5296
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2022-10-28 17:22:01 -0400

    Add rust impl

--- implementations/rust/src/main.rs
+++ implementations/rust/src/main.rs
@@ -1,3 +1,65 @@
+use std::{
+    env, fs,
+    io::{self, Write},
+};
+
+struct Shell {
+    cwd: String,
+}
+
 fn main() {
-    println!("hello world");
+    let mut exit = false;
+    let cwd = env::current_dir().unwrap().as_path().display().to_string();
+    let mut shell = Shell { cwd };
+    while !exit {
+        exit = prompt(&mut shell);
+        println!("\n")
+    }
+}
+
+fn prompt(shell: &mut Shell) -> bool {
+    print!("${}> ", shell.cwd);
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read command");
+    return read_command(&input, shell);
+}
+
+fn read_command(command: &String, shell: &mut Shell) -> bool {
+    let args: Vec<&str> = command.split(" ").map(|x| x.trim()).collect();
+    match args[0] {
+        "exit" => return true,
+        "ls" => list(args, shell),
+        "cd" => chdir(args, shell),
+        _ => {
+            print!("command not found: {}", args[0]);
+        }
+    }
+    return false;
+}
+
+fn chdir(args: Vec<&str>, shell: &mut Shell) {
+    if !(args.len() > 1) {
+        print!("cd expects a directory")
+    }
+    match env::set_current_dir(args[1]) {
+        Err(_) => print!("Cannot change directory to {}", args[1]),
+        _ => (),
+    }
+    shell.cwd = env::current_dir().unwrap().as_path().display().to_string();
+}
+
+fn list(_args: Vec<&str>, shell: &Shell) {
+    let paths = fs::read_dir(&shell.cwd).unwrap();
+
+    for path in paths {
+        let mut slash = "";
+        let file = path.unwrap();
+        if file.metadata().unwrap().is_dir() {
+            slash = "/";
+        }
+        println!("{}{}", file.file_name().to_str().unwrap(), slash)
+    }
 }
